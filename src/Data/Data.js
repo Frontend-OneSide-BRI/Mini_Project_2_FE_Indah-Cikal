b1d6f7cd046c5fee6c06a22c0b223c1cb7784fb4
Indah Cikal A O <indahcikalao@gmail.com>
2023-06-02 12:27:09 +0700

    initialize project

---
+++ src/Data/Data.js
@@ -0,0 +1,175 @@
+export const category = ["Most Popular", "Personal Favorite", "Best Artwork"];
+
+const data = [
+  {
+    id: 1,
+    src:
+      "https://images.unsplash.com/photo-1532767153582-b1a0e5145009?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    title: "Full Moon",
+    category: category[2],
+  },
+  {
+    id: 2,
+    src:
+      "https://images.unsplash.com/photo-1536195892759-c8a3c8e1945e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+    title: "Blue Sky",
+    category: category[2],
+  },
+  {
+    id: 3,
+    src:
+      "https://images.unsplash.com/photo-1585910165033-3facaf6984d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80",
+    title: "Sunset",
+    category: category[0],
+  },
+  {
+    id: 4,
+    src:
+      "https://images.unsplash.com/photo-1605377740297-bf87e7d3fdda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80",
+    title: "Mountain View",
+    category: category[0],
+  },
+  {
+    id: 5,
+    src:
+      "https://images.unsplash.com/photo-1504801861699-85070797b8f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    title: "Seashore",
+    category: category[1],
+  },
+  {
+    id: 6,
+    src:
+      "https://images.unsplash.com/photo-1498661873993-e39dc7ba2114?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    title: "Droplets",
+    category: category[1],
+  },
+  {
+    id: 7,
+    src:
+      "https://plus.unsplash.com/premium_photo-1675860537890-6f956cf9b122?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    title: "Beach",
+    category: category[0],
+  },
+  {
+    id: 8,
+    src:
+      "https://images.unsplash.com/photo-1536401780-267bfc37b2b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
+    title: "Palm Tree",
+    category: category[1],
+  },
+  {
+    id: 9,
+    src:
+      "https://images.unsplash.com/photo-1509114397022-ed747cca3f65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
+    title: "Orange Sky",
+    category: category[1],
+  },
+
+  {
+    id: 10,
+    src:
+      "https://images.unsplash.com/photo-1499063078284-f78f7d89616a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
+    title: "Airplane",
+    category: category[2],
+  },
+  {
+    id: 11,
+    src:
+      "https://plus.unsplash.com/premium_photo-1667424094018-d54ce121be62?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
+    title: "Ocean Cliff",
+    category: category[1],
+  },
+  {
+    id: 12,
+    src:
+      "https://plus.unsplash.com/premium_photo-1667326488721-55c127031b76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    title: "City",
+    category: category[2],
+  },
+  {
+    id: 13,
+    src:
+      "https://images.unsplash.com/photo-1612296494985-b03a50b0fcd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    title: "Brigde",
+    category: category[2],
+  },
+  {
+    id: 14,
+    src:
+      "https://images.unsplash.com/photo-1505118380757-91f5f5632de0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=326&q=80",
+    title: "Blue",
+    category: category[2],
+  },
+  {
+    id: 15,
+    src:
+      "https://images.unsplash.com/photo-1607923678396-a3e9ac34d70e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    title: "Above the Sea",
+    category: category[0],
+  },
+  {
+    id: 16,
+    src:
+      "https://images.unsplash.com/photo-1611642028692-6222b05b17ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+    title: "Sea",
+    category: category[1],
+  },
+  {
+    id: 17,
+    src:
+      "https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    title: "Trees",
+    category: category[1],
+  },
+  {
+    id: 18,
+    src:
+      "https://plus.unsplash.com/premium_photo-1670591909035-df1e99c163df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80",
+    title: "Pink Beach",
+    category: category[1],
+  },
+  {
+    id: 19,
+    src:
+      "https://images.unsplash.com/photo-1533661537256-701c0084511d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=405&q=80",
+    title: "Home",
+    category: category[0],
+  },
+  {
+    id: 20,
+    src:
+      "https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    title: "Home",
+    category: category[2],
+  },
+  {
+    id: 21,
+    src:
+      "https://images.unsplash.com/photo-1576051511569-38d504983e56?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    title: "Branch",
+    category: category[0],
+  },
+  {
+    id: 22,
+    src:
+      "https://plus.unsplash.com/premium_photo-1670422126816-fd0594135bb3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=917&q=80",
+    title: "GreenLand",
+    category: category[2],
+  },
+  {
+    id: 23,
+    src:
+      "https://images.unsplash.com/photo-1599939571322-792a326991f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=726&q=80",
+    title: "Blue Moon",
+    category: category[0],
+  },
+  {
+    id: 24,
+    src:
+      "https://images.unsplash.com/photo-1580457957853-ac7c1995b0a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    title: "Moon",
+    category: category[0],
+  },
+];
+
+export default data;
